b9e68250719b3b5a7f1988653c21f0e8509cf6f9
MurphyKlein <murphy.klein@gmail.com>
2026-04-28 15:46:45 -0400

    Debugging

--- supervised/train_val/train_function.py
+++ supervised/train_val/train_function.py
@@ -2,15 +2,8 @@ def train_one_epoch(model, dataloader, optimizer, criterion, device):
     model.train()
     total_loss = 0
 
-    for i, (batch_x, batch_y) in enumerate(dataloader):
+    for batch_x, batch_y in dataloader:
         batch_x, batch_y = batch_x.to(device), batch_y.to(device)
-
-        if i == 0:
-            print("batch_x device:", batch_x.device)
-            print("batch_y device:", batch_y.device)
-            print("model device:", next(model.parameters()).device)
-            print("patch_embedding device:", model.patch_embedding.weight.device)
-            print("pos_embedding device:", model.pos_embedding.device)
 
         optimizer.zero_grad()
         outputs = model(batch_x)
